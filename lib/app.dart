8809abf6f15aebcfb33a10cf22cb2c38372121b0
Kasun Lakshitha <kasun2014@outlook.com>
2023-11-18 23:27:41 +0530

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:outflow_v2/view/screen/home_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color(0xffffffff),
+            foregroundColor: Color(0xff202020),
+            shadowColor: Color(0x00202020),
+          )),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:outflow_v2/app.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
